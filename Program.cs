31747c1295ea2789276517b251a636077598921a
LeanMatoshi <l.matoshi.im26@stud.bbbaden.ch>
2026-08-28 16:54:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Lean");
+Console.WriteLine("Wie heisst du?");
+ string name = Console.ReadLine();
+Console.WriteLine($"Hallo, {name}!");
+Random zufallsgenerator = new Random();
+int geheimzahl = zufallsgenerator.Next(1, 100); 
+Console.WriteLine($"Debug: Die geheime Zahl ist {geheimzahl}");
+bool erraten = false; while (!erraten); 
